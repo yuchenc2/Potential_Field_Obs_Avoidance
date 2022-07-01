bf76ece1ac825ba991173e443a7555740eeebb91
Johnny Chang <yuchenc2@illinois.edu>
2022-07-01 09:10:39 -0500

    working on static force problem

--- Mujoco-VR-htcvive/mujoco-htcvive/src/satyyr_main.cpp
+++ Mujoco-VR-htcvive/mujoco-htcvive/src/satyyr_main.cpp
@@ -60,8 +60,8 @@ float *gaze;
 
 /*   Decide cases for feedback  */
 // #define CASE1_WITHOUT_FEEDBACK  /// 1
-#define CASE2_FEEDBACK_TO_HUMAN // 2
-// #define CASE3_COMPENSATED_CONTROLLER  // 3
+// #define CASE2_FEEDBACK_TO_HUMAN // 2
+#define CASE3_COMPENSATED_CONTROLLER  // 3
 // #define CASE4_COMPENSATED_CONTROLLER_WITH_FEEDBACK_TO_HUMAN // 4
 
 /* Map Cases */
