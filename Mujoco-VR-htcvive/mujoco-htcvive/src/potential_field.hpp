a269bf0a443f65b4e82a41d3b9df9bb75135aca5
Johnny Chang <yuchenc2@illinois.edu>
2022-07-11 14:53:31 -0500

    Fixed map

--- Mujoco-VR-htcvive/mujoco-htcvive/src/potential_field.hpp
+++ Mujoco-VR-htcvive/mujoco-htcvive/src/potential_field.hpp
@@ -20,8 +20,8 @@ using namespace std;
 // #define CASE4_COMPENSATED_CONTROLLER_WITH_FEEDBACK_TO_HUMAN // 4
 
 /* Map Cases */
-// #define STATIC_MAP  // 1
-#define DYNAMIC_MAP     // 2
+#define STATIC_MAP  // 1
+// #define DYNAMIC_MAP     // 2
 
 static int trial = 1; // 1 2 3 4 5
 
